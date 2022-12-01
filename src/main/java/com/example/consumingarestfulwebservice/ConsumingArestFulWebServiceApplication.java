d8cce3b095bdf48dc1343eef30b2d2b92e028d4d
Shizuka Tanigawa <chocokkuma@gmail.com>
2022-12-01 13:54:57 +0100

    first commit

---
+++ src/main/java/com/example/consumingarestfulwebservice/ConsumingArestFulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumingarestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingArestFulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingArestFulWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/consumingarestfulwebservice/ConsumingArestFulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumingarestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingArestFulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
