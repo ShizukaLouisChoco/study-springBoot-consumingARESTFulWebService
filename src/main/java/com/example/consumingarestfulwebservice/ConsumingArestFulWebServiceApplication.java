45115deac5e0fe7eadc02c13cc3b52a5adc0cc72
Shizuka Tanigawa <chocokkuma@gmail.com>
2022-12-01 15:02:07 +0100

    1) Fetching a REST Resource

--- src/main/java/com/example/consumingarestfulwebservice/ConsumingArestFulWebServiceApplication.java
+++ src/main/java/com/example/consumingarestfulwebservice/ConsumingArestFulWebServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConsumingArestFulWebServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumingArestFulWebServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingArestFulWebServiceApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/consumingarestfulwebservice/ConsumingArestFulWebServiceApplicationTests.java
+++ src/test/java/com/example/consumingarestfulwebservice/ConsumingArestFulWebServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConsumingArestFulWebServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
